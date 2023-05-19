6fc414cce4f149e0dc30c7bc80efb71ed8b02473
Lula Lavinia <128543971+LulaLavinia@users.noreply.github.com>
2023-05-19 17:29:52 -0300

    Add files via upload

---
+++ assets/scenes/Preload.js
@@ -0,0 +1,42 @@
+import {SHAPES} from '../../utils.js';
+const{TRIANGLE, SQUARE, DIAMOND, REDTRIANGLE, DOLAR, PESO} = SHAPES;
+export default class Preload extends Phaser.Scene {
+constructor() {
+    super("Preload");
+}
+
+preload(){
+    this.load.image("sky", "./assets/images/Sky.png");
+    this.load.image("ground", "./assets/images/platform.png");
+    this.load.image("ninja", "./assets/images/Ninja.png");
+    this.load.image(SQUARE, "./assets/images/Square.png");
+    this.load.image(DIAMOND, "./assets/images/Diamond.png");
+    this.load.image(TRIANGLE, "./assets/images/Triangle.png");
+    this.load.image("moon", "./assets/images/moon4.png");
+    this.load.image(REDTRIANGLE, "./assets/images/redTriangle.png");
+    this.load.image(DOLAR, "./assets/images/dolar.png");
+    this.load.image(PESO, "./assets/images/peso.png");
+    
+    
+
+    this.load.image("win2", "./assets/images/Win2.png");
+    this.load.image("gameOver", "./assets/images/GameOver.png");
+
+
+}
+
+create() {
+    this.scene.start("Game");
+}
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ utils.js
@@ -0,0 +1,20 @@
+//export default class utils ex
+const SHAPES = {
+    TRIANGLE: 'triangle',
+    SQUARE: 'square',
+    DIAMOND: 'diamond',
+    DOLAR :'dolar',
+    REDTRIANGLE: 'redTriangle',
+    PESO: 'peso',
+
+}
+
+const POINTS_PERCENTAGE = "value";
+const POINTS_PERCENTAGE_VALUE_START = 1;
+
+
+export {
+    SHAPES,
+    POINTS_PERCENTAGE,
+    POINTS_PERCENTAGE_VALUE_START,
+}
